e3948b2a718305e9f12269aa60a09d468c08002e
MechTech2sunny <mechtech.sunny@gmail.com>
2022-08-02 22:53:08 +0530

    my first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Resto from './component/Basics/Resto';
+// import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+import Todo from './component/ToDoReact/todo';
+
+const App = () => {
+  return <Todo/>
+};
+
+export default App;
