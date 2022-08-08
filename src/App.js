b424be09e9223f3684c48bd38ca21225a8beef93
MechTech2sunny <mechtech.sunny@gmail.com>
2022-08-08 23:00:42 +0530

    my second commit react

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 // import UseState from './component/Hooks/useState';
 // import UseEffect from './component/Hooks/useEffect';
 // import UseReducer from './component/Hooks/useReducer';
-// import Todo from './component/ToDoReact/todo';
-import Temp from './component/weather/temp';
+import Todo from './component/ToDoReact/todo';
+// import Temp from './component/weather/temp';
 
 const App = () => {
-  return <Temp/>
+  return <Todo/>
 };
 
 export default App;
